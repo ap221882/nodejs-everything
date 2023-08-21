ca36ea4c7dbf852b504068289e6691aef205c864
ajay <ajayp@naehas.com>
2023-08-21 21:49:55 +0530

    initial script

---
+++ digging-into-node/exercises/4-smart-runscript.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+"use strict";
+
+//^ 1. printHelp();
+
+function printHelp() {
+  console.log("ex1 usage: ");
+  console.log("    ex1.js --help");
+  console.log("");
+  console.log("--help                 print this help");
+  console.log("");
+}
