9d7f7630763610e892e94a2c2e6d4e3eabb83dfc
ajay <ajayp@naehas.com>
2023-08-22 10:09:23 +0530

    argument handling done

--- digging-into-node/exercises/4-smart-runscript.js
+++ digging-into-node/exercises/4-smart-runscript.js
@@ -6,8 +6,35 @@
 
 function printHelp() {
   console.log("ex1 usage: ");
-  console.log("    ex1.js --help");
+  console.log("    ex1.js --file={FILENAME}");
   console.log("");
   console.log("--help                 print this help");
+  console.log("--file={FILENAME}      process this file");
   console.log("");
 }
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
+
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help"],
+  string: ["file"],
+});
+console.log(args);
+
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  console.log(args.file);
+} else {
+  error("Incorrect usage", true);
+}
+
+//~% run -  ./4-smart-runscript.js --mode=development -c9 - foo bar (_ is for things which minimist doesn't understand)
+
+//~? try https://yargs.js.org/ for reference - it uses minimist - is a wrapper around minimist
