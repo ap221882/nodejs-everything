3199513a7267a7b0e557d2da8b82447aecfae51a
ajay <ap221882@gmail.com>
2023-08-23 20:33:55 +0530

    server accepting stream

--- digging-into-node/streams/3-concat-streams.js
+++ digging-into-node/streams/3-concat-streams.js
@@ -1,11 +1,27 @@
 var concat = require("concat-stream");
 var http = require("http");
+var qs = require("querystring");
 
-process.stdin.pipe(
-  concat(function (body) {
-    console.log(body.length);
-  })
-);
+// process.stdin.pipe(
+//   concat(function (body) {
+//     console.log(body.length);
+//   })
+// );
 //> node "file" then type words press enter and enter and at last press ctrl+D
 //* ctrl D from stdin, we tell the program that youre finished typing
 //* ctrl C is used to kill the program
+
+var server = http.createServer((req, res) => {
+  req.pipe(
+    concat({ encoding: "string" }, (body) => {
+      // console.log(body.length);
+      var params = qs.parse(body);
+      console.log(params);
+      res.end("ok\n");
+    })
+  );
+});
+//> run -> curl -d msg=hello localhost:8000
+ok;
+
+server.listen(8000);
