ac03613221dee921c3b1ab1b878bd545ee3b9dce
ajay <ap221882@gmail.com>
2023-08-23 14:39:38 +0530

    uppercase the content

--- digging-into-node/streams/1.js
+++ digging-into-node/streams/1.js
@@ -1,7 +1,17 @@
 var fs = require("fs");
 var path = require("path");
+var through = require("through2");
 
 //>PIPE
-fs.createReadStream(path.join(__dirname, "../exercises/files/lorem.txt")).pipe(
-  process.stdout
-);
+// fs.createReadStream(path.join(__dirname, "../exercises/files/lorem.txt")).pipe(
+//   process.stdout
+// );
+
+//> Process the buffer data
+
+fs.createReadStream(process.argv[2]).pipe(through(write)).pipe(process.stdout);
+
+function write(buffer, encoding, next) {
+  //> what is returned from the buffer
+  next(null /* > error */, buffer.toString().toUpperCase());
+}
